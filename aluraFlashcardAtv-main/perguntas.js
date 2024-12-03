c705b8a00de3b3f779b6102c11a5e00018ae8c06
Jovem-Marcos <marcossoarescarvalho97@gmail.com>
2024-12-03 17:44:06 -0300

    Add files via upload

---
+++ aluraFlashcardAtv-main/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Marketing',
+    'Qual o significado de CRM?',
+    'Customer Relationship Management.'
+)
+
+criaCartao(
+    'Design',
+    'Qual o significado da cor vermelha?',
+    'Geralmente é representada por amor, raiva, sedução ou perigo!'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Onde se iniciou a primeira revolução industrial?',
+    'Na Inglaterra.'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais sãos as 4 partes principais do Sangue?',
+    'Glóbulos vermelos, glóbulos brancos, plaquetas e plasma.'
+)
